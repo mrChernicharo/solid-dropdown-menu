c719485be76f90f2a1d1d49eedb201ee54e64af9
Felipe Chernicharo <string7dev@gmail.com>
2022-06-29 19:25:03 -0400

    it works !!

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,9 +1,8 @@
-import { createEffect, createSignal, onCleanup } from 'solid-js';
+import { createEffect, createSignal, onCleanup, onMount } from 'solid-js';
 import s from './App.module.css';
 
 export default function Menu(props) {
     let i = 0
-    let j = 0
     const [openSubMenus, setOpenSubMenus] = createSignal([]);
     const depth = () => props.options[0].depth
 
@@ -12,14 +11,18 @@ export default function Menu(props) {
     createEffect(() => console.log(JSON.stringify(openSubMenus(), null, 2)))
 
     createEffect(() => {
+        console.log(openSubMenus())
+    })
+
+    onMount(() => {
 
         setOpenSubMenus(() => {
 
             const subMenusState = []
-            props.options.forEach(opt => {
+            props.options.forEach((opt, i) => {
                 console.log(opt)
                 if (opt.options) {
-                    subMenusState.push(false)
+                    subMenusState.push({ id: i, open: false })
                 }
             })
             return subMenusState
@@ -65,27 +68,27 @@ export default function Menu(props) {
                                 onclick={e => {
                                     // console.log(option, i)
                                     setOpenSubMenus((sms) => {
-
-                                        const bools = [...sms]
-                                        // const bool = bools[i]
-                                        // bools.splice(i, 1, bool)
-                                        console.log({ bools, index: props.index, i: i, j: j++, depth: depth(), idx: idx() })
-                                        return bools
+                                        return sms.map(menu => {
+                                            if (menu.id === idx()) {
+                                                return { ...menu, open: !menu.open }
+                                            }
+                                            return menu
+                                        })
                                     })
-                                    // setIsSubMenuOpen(!isSubMenuOpen());
+
                                 }}
                             >
                                 {el(option, idx())} ‣
                             </div>
 
-                            {/* <Show when={isSubMenuOpen()}> */}
-                            <Menu
-                                index={i++}
-                                options={option.options}
-                                setValue={props.setValue}
-                                setIsOpen={props.setIsOpen}
-                            />
-                            {/* </Show> */}
+                            <Show when={openSubMenus()[openSubMenus().findIndex(m => m.id === idx())]?.open}>
+                                <Menu
+                                    index={i++}
+                                    options={option.options}
+                                    setValue={props.setValue}
+                                    setIsOpen={props.setIsOpen}
+                                />
+                            </Show>
                         </Show>)
                 }}
             </For>
